a075c00b4de41035b5e15af494662e58bbd6408c
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 15:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
